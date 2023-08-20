2f69e84d20442012c8470d204bff1f97cd013429
Shorya Rastogi <49874404+shorya2201@users.noreply.github.com>
2023-08-20 19:09:04 +0530

    Task 1 : Number Guessing Game
    It is a very Basic version of the game and can be further modified.

---
+++ NumberGuessGame.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class NumberGuessGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Generate the number
+        int number = 1 + (int) (100 * Math.random());// 100 indicates the upper bound & 1 indicates lower bound
+
+        int i, guess;
+
+        // This indicates the number of trials we are giving to player
+        System.out.println("Enter the Number of Trials : ");
+        int trials = sc.nextInt();
+
+        System.out.println("A number is guessed between 1 to 100 , Guess a number in " + trials + " trials.");
+
+        for (i = 0; i < trials; i++) {
+            System.out.println("Guess a Number: ");
+            guess = sc.nextInt();
+            if (guess == number) {
+                System.out.println("Congratulations! You have Successfully Guessed the Number!! ");
+                break; // Exits the loop
+            } else if (guess < number) {
+                System.out.println("The Number is Greater than " + guess);
+            } else if (guess > number) {
+                System.out.println("The Number is Smaller than " + guess);
+            }
+        }
+
+        if (i == trials) { // If trials are exhausted and player loses
+            System.out.println("You have exhausted " + trials + " trials.");
+            System.out.println("You Lose !! The Correct number was " + number);
+        }
+    }
+}
